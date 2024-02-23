94ee664cfdd64c618bff48d39ec0b6adb17c0ef8
thangpaisen <thangpaisen@gmail.com>
2024-02-23 10:14:06 +0700

    update

--- src/Containers/Home/HomeScreen.tsx
+++ src/Containers/Home/HomeScreen.tsx
@@ -1,16 +1,7 @@
 import { Colors } from '@/Constants'
 import { useListMeme } from '@/Hooks/useListMeme'
-import { SCREEN_WIDTH } from '@/Utils/common'
 import React from 'react'
-import {
-  Dimensions,
-  FlatList,
-  SafeAreaView,
-  StyleSheet,
-  TouchableOpacity,
-  View,
-} from 'react-native'
-import Image from 'react-native-fast-image'
+import { ActivityIndicator, FlatList, StyleSheet, View } from 'react-native'
 import CardImage from './Components/CardImage'
 import Header from './Components/Header'
 
@@ -21,22 +12,30 @@ const HomeScreen = () => {
     return <CardImage data={item} />
   }
 
+  const renderLoading = () => {
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicator size={'small'} color={'blue'} />
+      </View>
+    )
+  }
+
   return (
-    //     <SafeAreaView style={{ flex: 1 }}>
     <View style={styles.container}>
       <Header />
       <FlatList
         data={data}
-        keyExtractor={(item) => item?.id}
+        keyExtractor={(item) => item.id}
         renderItem={renderItem}
         numColumns={2}
         contentContainerStyle={{
           paddingHorizontal: 10,
+          flexGrow: 1,
         }}
         showsVerticalScrollIndicator={false}
+        ListEmptyComponent={renderLoading}
       />
     </View>
-    //     </SafeAreaView>
   )
 }
 
@@ -46,5 +45,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.white,
+  },
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 })
